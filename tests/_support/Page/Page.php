fc8c5443580ba55bef4a01940f085f44511f5f72
Roland Golla <rolandgolla@gmail.com>
2020-06-07 00:46:37 +0200

    Add wikipedia search autocomplete cest

---
+++ tests/_support/Page/Page.php
@@ -0,0 +1,11 @@
+<?php
+namespace Tests\Page;
+
+class Page
+{
+    public static $startpagePath = '/';
+    public static $startpageSearchInput = '#txtSearch';
+    public static $startpageSearchAutocompleteDiv = '#search_suggest';
+    public static $startpageSearchAutocompleteItemsSpanClass = '.search_result';
+}
+
---
+++ tests/acceptance/Search/SearchCest.php
@@ -0,0 +1,29 @@
+<?php 
+namespace Tests\Search;
+
+use Tests\AcceptanceTester;
+use Tests\Page\Page;
+
+class SearchCest
+{
+    public function _before(AcceptanceTester $I, Page $page)
+    {
+        $I->amOnPage($page::$startpagePath);
+        $I->waitForElement($page::$startpageSearchInput);
+    }
+
+    // tests
+    public function autocompleteItemsMatchSearchWord(AcceptanceTester $I, Page $page)
+    {
+        $searchWord = 'code';
+        $I->fillField($page::$startpageSearchInput, $searchWord);
+        $I->waitForElementVisible($page::$startpageSearchAutocompleteDiv);
+        $spans = $I->grabMultiple($page::$startpageSearchAutocompleteItemsSpanClass);
+
+        $I->assertNotEmpty($spans, 'Autocomplete has items');
+
+        foreach ($spans as $key => $span) {
+            $I->assertStringContainsString($searchWord, strtolower($span), 'Key: ' . $key);
+        }
+    }
+}
